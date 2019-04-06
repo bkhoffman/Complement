c81db5018b05d22bcc5ab78ebf4ab288153c9ce9
Tyler Ward <T.ward26@gmail.com>
2019-04-06 10:13:44 -0500

    Added data-attributes to card

--- edamam.js
+++ edamam.js
@@ -1,11 +1,11 @@
 
 $("#recipeSearch").click(function(event){
+    event.preventDefault();
     var string = $("#inputIngredients").val().trim();
     var firstWord = string.substring(0, string.indexOf(" "));
     console.log(firstWord);
     var searchString = string.replace(" ", ",");
     console.log("click");
-    event.preventDefault();
     edamamCall(searchString);
     $("#inputIngredients").val("");
 });
@@ -22,29 +22,21 @@ function edamamCall(searchString) {
             for(var i=0; i<recipeArray.length; i++){
                 var card = $("<div>");
                 card.addClass("card recipeCard");
+                card.attr("data-label", `${recipeArray[i].recipe.label}`);
+                card.attr("data-url", `${recipeArray[i].recipe.url}`);
+                card.attr("data-ingredients", `${recipeArray[i].recipe.ingredientLines}`);
+                card.attr("data-source", `${recipeArray[i].recipe.source}`);
+                card.attr("data-imageUrl", `${recipeArray[i].recipe.image}`);
 
                 var cardHeader = $("<div>").addClass("card-header");
                 cardHeader.text(recipeArray[i].recipe.label);
 
                 var cardBody = $("<div>").addClass("card-body");
-                cardBody.attr("id", `cardBody${i}`);
                 cardBody.html(`<img src="${recipeArray[i].recipe.image}" class="img-fluid mx-auto d-block">`);
                 
                 card.append(cardHeader, cardBody);
-                $(`#cardBody${i}`).wrap(`<a href='${recipeArray[i].recipe.url}'></a>`);
                 $("#results").append(card);
             }
-            $(`#cardBody0`).wrap(`<a href='${recipeArray[0].recipe.url}'></a>`);
-            $(`#cardBody1`).wrap(`<a href='${recipeArray[1].recipe.url}'></a>`);
-            $(`#cardBody2`).wrap(`<a href='${recipeArray[2].recipe.url}'></a>`);
-            $(`#cardBody3`).wrap(`<a href='${recipeArray[3].recipe.url}'></a>`);
-            $(`#cardBody4`).wrap(`<a href='${recipeArray[4].recipe.url}'></a>`);
-            $(`#cardBody5`).wrap(`<a href='${recipeArray[5].recipe.url}'></a>`);
-            $(`#cardBody6`).wrap(`<a href='${recipeArray[6].recipe.url}'></a>`);
-            $(`#cardBody7`).wrap(`<a href='${recipeArray[7].recipe.url}'></a>`);
-            $(`#cardBody8`).wrap(`<a href='${recipeArray[8].recipe.url}'></a>`);
-            $(`#cardBody9`).wrap(`<a href='${recipeArray[9].recipe.url}'></a>`);
-            
             //response.hits gives an array of 10 recipes
             //use a for loop to iterate throuh the 10 recipes looking at 
             //response.hits[0].recipe.image for the recipe image url
@@ -55,4 +47,7 @@ function edamamCall(searchString) {
 
         });
     }
-    // ajaxDemo();
+
+    $(".recipeCard").click(function(){
+        
+    });
